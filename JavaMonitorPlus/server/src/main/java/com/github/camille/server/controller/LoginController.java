f2da436ee5f252ff8aac040273307dcf3ad73f28
camille <pengkangzaia@foxmail.com>
2022-01-20 15:43:01 +0800

    调整前端登录页面

--- JavaMonitorPlus/server/src/main/java/com/github/camille/server/controller/LoginController.java
+++ JavaMonitorPlus/server/src/main/java/com/github/camille/server/controller/LoginController.java
@@ -1,7 +1,8 @@
 package com.github.camille.server.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * @author pengkangzaia@foxmail.com
@@ -10,6 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 @Controller
 public class LoginController {
 
+    @RequestMapping(value = "/mylogin", method = RequestMethod.GET)
+    public String login() {
+        return "login";
+    }
 
 
 
