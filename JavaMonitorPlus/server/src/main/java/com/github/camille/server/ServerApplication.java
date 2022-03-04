a4cef65813c0fd0375bfaedd0b19d909e534168f
camille <pengkangzaia@foxmail.com>
2022-03-04 19:51:07 +0800

    引入consul实现服务注册与发现

--- JavaMonitorPlus/server/src/main/java/com/github/camille/server/ServerApplication.java
+++ JavaMonitorPlus/server/src/main/java/com/github/camille/server/ServerApplication.java
@@ -4,11 +4,14 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
-@EnableScheduling
+
 @EnableCaching
+@EnableScheduling
+@EnableDiscoveryClient
+@SpringBootApplication
 @MapperScan("com/github/camille/server/database/dao")
 public class ServerApplication {
 
